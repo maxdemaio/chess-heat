e3e654735a379d955b38b67e3cd6e485c84e6316
maxdemaio <maxdman321@gmail.com>
2023-03-27 13:25:47 -0400

    prettier cleanup

--- script.js
+++ script.js
@@ -1,61 +1,54 @@
 /* Create svgs day boxes for each month (default as current year) */
-const maxDaysInMonthArr = [31,29,31,30,31,30,31,31,30,31,30,31];
+const maxDaysInMonthArr = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
 for (let i = 0; i < maxDaysInMonthArr.length; i++) {
-
   // Get the element with the heatmap-id="0"
   const heatmapElement = document.querySelector(`[heatmap-id="${i}"]`);
 
-  console.log(maxDaysInMonthArr);
-  console.log(heatmapElement);
-
   for (let j = 0; j < maxDaysInMonthArr[i]; j++) {
-      // Create the elements using the createElement() method
-      const dayBoxContainer = document.createElement('div');
-      const svgElement = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-      const rectElement = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
-      
-      // Add the classes and attributes to the elements
-      dayBoxContainer.classList.add('dayBoxContainer');
-      svgElement.classList.add('dayBox', 'grayBox');
-      rectElement.classList.add('fill-current');
-      rectElement.setAttribute('width', '100%');
-      rectElement.setAttribute('height', '100%');
-      
-      svgElement.setAttribute("daybox-id", j);
-      const popup = document.createElement('div');
-      popup.classList.add('popup-content');
-      popup.setAttribute("popup-id", j)
+    // Create the elements using the createElement() method
+    const dayBoxContainer = document.createElement("div");
+    const svgElement = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    const rectElement = document.createElementNS("http://www.w3.org/2000/svg", "rect");
 
-      // Hide the popup span initially
-      popup.style.display = 'none';
+    // Add the classes and attributes to the elements
+    dayBoxContainer.classList.add("dayBoxContainer");
+    svgElement.classList.add("dayBox", "grayBox");
+    rectElement.classList.add("fill-current");
+    rectElement.setAttribute("width", "100%");
+    rectElement.setAttribute("height", "100%");
 
-      // Add mouse enter listener to show the popup span
-      svgElement.addEventListener('mouseenter', () => {
-        popup.style.display = 'inline';
-      });
+    svgElement.setAttribute("daybox-id", j);
+    const popup = document.createElement("div");
+    popup.classList.add("popup-content");
+    popup.setAttribute("popup-id", j);
 
-      // Add mouse leave listener to hide the popup span
-      svgElement.addEventListener('mouseleave', () => {
-        popup.style.display = 'none';
-      });
+    // Hide the popup span initially
+    popup.style.display = "none";
 
-      // Append the elements to each other
-      svgElement.appendChild(rectElement);
-      dayBoxContainer.appendChild(svgElement);
-      svgElement.insertAdjacentElement('afterend', popup);
+    // Add mouse enter listener to show the popup span
+    svgElement.addEventListener("mouseenter", () => {
+      popup.style.display = "inline";
+    });
 
-      heatmapElement.appendChild(dayBoxContainer);
+    // Add mouse leave listener to hide the popup span
+    svgElement.addEventListener("mouseleave", () => {
+      popup.style.display = "none";
+    });
+
+    // Append the elements to each other
+    svgElement.appendChild(rectElement);
+    dayBoxContainer.appendChild(svgElement);
+    svgElement.insertAdjacentElement("afterend", popup);
+
+    heatmapElement.appendChild(dayBoxContainer);
   }
 }
-
-
-
 
 /* Chess.com API Logic */
 async function fetchData(user, year) {
   // Clear out previous data and start pulsate
-  const monthDaySquaresPrev = document.querySelectorAll('.dayBox');
+  const monthDaySquaresPrev = document.querySelectorAll(".dayBox");
   console.log(monthDaySquaresPrev);
   monthDaySquaresPrev.forEach((node) => {
     node.classList.remove("greenBox");
@@ -75,8 +68,8 @@ async function fetchData(user, year) {
         day: j,
         wins: 0,
         losses: 0,
-        draws: 0
-      }
+        draws: 0,
+      };
       temp.push(monthDayMeta);
     }
     monthDayMetaArr.push(temp);
@@ -86,7 +79,7 @@ async function fetchData(user, year) {
 
   // Query the chesscom API and create an array of promises
   const baseUrl = `https://api.chess.com/pub/player/${user}/games/${year}/`;
-  const months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
+  const months = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
 
   // Truncate months if it's the current year
   const currentYear = new Date().getFullYear();
@@ -95,14 +88,13 @@ async function fetchData(user, year) {
     months = months.slice(0, currentMonth); // extract portion of the months array up to the current month
   }
 
-
   for (const [index, month] of months.entries()) {
     // Get current month's heatmap
     // Select all elements with the class name ".dayBox" within the heatmapElement
     const heatmapElement = document.querySelector(`[heatmap-id="${index}"]`);
-    const heatmapMonthDaySquares = heatmapElement.querySelectorAll('.dayBox');
+    const heatmapMonthDaySquares = heatmapElement.querySelectorAll(".dayBox");
 
-    const url = baseUrl + month + '/pgn';
+    const url = baseUrl + month + "/pgn";
     try {
       const response = await fetch(url);
       // end pulsate
@@ -110,16 +102,15 @@ async function fetchData(user, year) {
         node.classList.remove("pulsate");
       });
       if (!response.ok) {
-        throw new Error('Failed to fetch data');
+        throw new Error("Failed to fetch data");
       }
       const data = await response.text();
 
       console.log("Sending request for month with index: ", index);
-      console.log("monthDayMeta for month index: ", monthDayMetaArr[index])
-      console.log("--------")
+      console.log("monthDayMeta for month index: ", monthDayMetaArr[index]);
+      console.log("--------");
 
-
-      const pgns = data.split('\n\n\n');
+      const pgns = data.split("\n\n\n");
       // iterate over the pgn annotations
       for (let i = 0; i < pgns.length; i++) {
         const currPgn = pgns[i];
@@ -145,39 +136,36 @@ async function fetchData(user, year) {
         console.log("annotations: ", annotations);
 
         // If these specific annotations aren't found on the pgn, skip
-        if (!annotations.hasOwnProperty('Black')
-          || !annotations.hasOwnProperty('White')
-          || !annotations.hasOwnProperty('Date')
-          || !annotations.hasOwnProperty('Result')) {
+        if (!annotations.hasOwnProperty("Black") || !annotations.hasOwnProperty("White") || !annotations.hasOwnProperty("Date") || !annotations.hasOwnProperty("Result")) {
           console.log("Needed annotations not found on current PGN");
           continue;
         }
 
         // Lowercase the player names
-        annotations.Black = annotations.Black.toLowerCase()
-        annotations.White = annotations.White.toLowerCase()
+        annotations.Black = annotations.Black.toLowerCase();
+        annotations.White = annotations.White.toLowerCase();
 
         // Extract day integer from the date property
         // There may be many matches on the same day
         // So, we use that as the index
         // We subtract 1 because the array is 0-indexed
-        const day = parseInt(annotations.Date.split('.')[2]) - 1;
+        const day = parseInt(annotations.Date.split(".")[2]) - 1;
 
         // update meta
         if (annotations.Black === user && annotations.Result === "0-1") {
-          monthDayMetaArr[index][day].wins += 1
+          monthDayMetaArr[index][day].wins += 1;
         }
         if (annotations.Black === user && annotations.Result === "1-0") {
-          monthDayMetaArr[index][day].losses += 1
+          monthDayMetaArr[index][day].losses += 1;
         }
         if (annotations.White === user && annotations.Result === "1-0") {
-          monthDayMetaArr[index][day].wins += 1
+          monthDayMetaArr[index][day].wins += 1;
         }
         if (annotations.White === user && annotations.Result === "0-1") {
-          monthDayMetaArr[index][day].losses += 1
+          monthDayMetaArr[index][day].losses += 1;
         }
         if (annotations.Result === "1/2-1/2") {
-          monthDayMetaArr[index][day].draws += 1
+          monthDayMetaArr[index][day].draws += 1;
         }
       }
 
@@ -191,43 +179,35 @@ async function fetchData(user, year) {
 
           // update popup content
           const popup = heatmapElement.querySelector(`[popup-id="${i}"]`);
-          popup.classList.add("popup-exists")
+          popup.classList.add("popup-exists");
           popup.textContent = `day ${i + 1}, wins ${monthDayMetaArr[index][i].wins}, losses ${monthDayMetaArr[index][i].losses}, draws ${monthDayMetaArr[index][i].draws}`;
         }
       }
     } catch (error) {
       console.error(`Error has occurred: ${error.message}`);
     }
-
-
   }
 }
 
-
-
-
 /* Form logic */
-document.getElementById('form').addEventListener('submit', (e) => {
+document.getElementById("form").addEventListener("submit", (e) => {
   // prevent the form from refreshing the page
   e.preventDefault();
 
   // get the value of the input
-  const user = document.getElementById('form-input-user').value;
-  const year = document.getElementById('form-input-year').value;
+  const user = document.getElementById("form-input-user").value;
+  const year = document.getElementById("form-input-year").value;
 
   // validate year
   if (year < 2008) {
-    alert('Year must be greater than 2007.');
+    alert("Year must be greater than 2007.");
     return;
   }
   if (year > new Date().getFullYear()) {
-    alert('Year must not be in the future.');
+    alert("Year must not be in the future.");
     return;
   }
 
   // call the function that handles the Chess.com requests
   fetchData(user, year);
 });
-
-
-

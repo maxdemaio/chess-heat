a1d16f9a5bbb5d819ad6833adf62e421c846c7ab
maxdemaio <maxdman321@gmail.com>
2023-06-13 19:03:28 -0400

    add JSDoc as needed

--- time-classes.js
+++ time-classes.js
@@ -1,5 +1,19 @@
+/**
+ * An array of valid time classes.
+ * @type {string[]}
+ */
 const VALID_TIME_CLASSES = Object.freeze(['bullet', 'blitz', 'rapid', 'daily']);
+
+/**
+ * The ID for the filter select element.
+ * @type {string}
+ */
 const FILTER_SELECT_ID = 'filter-select';
+
+/**
+ * The value representing any option.
+ * @type {string}
+ */
 const ANY_OPTION_VALUE = '';
 
 /**
@@ -8,24 +22,41 @@ const ANY_OPTION_VALUE = '';
  * @param {string} timeCLass - Time class. One of a VALID_TIME_CLASSES
  * @returns {array} - New array filtered by passed time class
  */
-const filterGamesByTimeClass = (games, timeCLass) => {
+function filterGamesByTimeClass(games, timeCLass) {
   return games.filter((game) => game.time_class === timeCLass);
-};
+}
 
-const generate = () => {
+/**
+ * Generates a select element and appends it to the filter box DOM element.
+ */
+function generate() {
+  /**
+   * The DOM element representing the filter box.
+   * @type {Element}
+   */
   const filterBoxDOM = document.querySelector('.filter');
 
+  /**
+   * The select element to be appended to the filter box DOM.
+   * @type {HTMLSelectElement}
+   */
   const select = document.createElement('select');
   select.name = 'filter';
   select.id = FILTER_SELECT_ID;
   const optionNodes = [];
 
-  const createOption = (value, text) => {
+  /**
+   * Creates an option element with the specified value and text content,
+   * and pushes it to the optionNodes array.
+   * @param {string} value - The value of the option.
+   * @param {string} text - The text content of the option.
+   */
+  function createOption(value, text) {
     const option = document.createElement('option');
     option.value = value;
     option.textContent = text;
     optionNodes.push(option);
-  };
+  }
 
   createOption(ANY_OPTION_VALUE, 'any');
 
@@ -33,21 +64,25 @@ const generate = () => {
 
   select.append(...optionNodes);
   filterBoxDOM.append(select);
-};
+}
 
-const reset = () => {
+function reset() {
   document.getElementById(FILTER_SELECT_ID).value = ANY_OPTION_VALUE;
-};
-const disable = () => {
+}
+function disable() {
   document.getElementById(FILTER_SELECT_ID).disabled = true;
-};
-const enable = () => {
+}
+function enable() {
   document.getElementById(FILTER_SELECT_ID).disabled = false;
-};
+}
 
-const createChangeEventListener = (func) => {
+/**
+ * Creates a change event listener on the filter select element.
+ * @param {function} func - The function to be called when the change event is triggered.
+ */
+function createChangeEventListener(func) {
   document.getElementById(FILTER_SELECT_ID).addEventListener('change', func);
-};
+}
 
 export const FilterOptions = {
   filterGamesByTimeClass,
